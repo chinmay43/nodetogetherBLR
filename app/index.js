9596c5e656cd5a86df9909a6afb90c8604823b12
Chinmay Gargava <chinmay.gargava@livquik.com>
2016-03-20 17:55:09 +0530

    Add randomizations in the motivations returned by implementing the pick-one module

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const motivations = require('motivations');
+const pickOne = require('pick-one');
 
 app.get('/', function(req, res) {
   res.send({one:"one",two:"two"});
 });
 
 app.get('/motivation', function(req, res) {
-  res.send(motivations[parseInt(Math.random() * (motivations.length -1))]);
+  //res.send(motivations[parseInt(Math.random() * (motivations.length -1))]);
+  res.send(pickOne(motivations));
 });
 
 module.exports = app;
